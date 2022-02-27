f32b8e4ec2011dbef231aa55ca6373bc8b0b729f
muntazir ahmed <muntazirahmed7@gmail.com>
2022-02-27 20:38:12 +0530

    questions

---
+++ index.js
@@ -0,0 +1,27 @@
+const quizdata=[
+    { 
+        question:' name of my country',
+        a:'india',
+        b:'pak',
+        c:'nepal',
+        d:'japan',
+        correct:'a'
+    },
+    { 
+        question:'largest state',
+        a:'up',
+        b:'mp',
+        c:'ap',
+        d:'maharastra',
+        correct:'a'
+
+    },
+    {
+    question:'smallest state',
+    a:'up',
+    b:'mp',
+    c:'goa',
+    d:'maharastra',
+    correct:'c'
+    }
+]
